049ed52e813dc035561441affe7a68ca83d59492
Paulo Soares <paulosoares@dhcp-10-142-253-1.uawifi.arizona.edu>
2020-01-18 20:04:18 -0700

    database

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import pyrebase
 import pymysql
-from flask import Flask, render_template, request
+import json
+from flask import Flask, render_template, request, g
 app = Flask(__name__)
 
 config = {
@@ -16,18 +17,17 @@ config = {
 
 db_user = "root"#os.environ.get('CLOUD_SQL_USERNAME')
 db_password = "cluivilab"#os.environ.get('CLOUD_SQL_PASSWORD')
-db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+db_name = "hackaz_db"#os.environ.get('CLOUD_SQL_DATABASE_NAME')
 db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
-
-def print_questions():
+def open_db_connection():
     if os.environ.get('GAE_ENV') == 'standard':
         # If deployed, use the local socket interface for accessing Cloud SQL
         unix_socket = '/cloudsql/{}'.format(db_connection_name)
-        cnx = pymysql.connect(user=db_user, password=db_password,
+        g.db = pymysql.connect(user=db_user, password=db_password,
                               unix_socket=unix_socket, db=db_name)
     else:
         # If running locally, use the TCP connections instead
@@ -35,22 +35,121 @@ def print_questions():
         # so that your application can use 127.0.0.1:3306 to connect to your
         # Cloud SQL instance
         host = '127.0.0.1'
-        cnx = pymysql.connect(user=db_user, password=db_password,
+        g.db = pymysql.connect(user=db_user, password=db_password,
                               host=host, db=db_name)
 
-    with cnx.cursor() as cursor:
-        cursor.execute('SELECT NOW() as now;')
-        result = cursor.fetchall()
-        current_time = result[0][0]
-    cnx.close()
+def close_db_connection():
+    if g.db is not None:
+        g.db.close()
 
-    print('yes')
-    print(str(current_time))
-	
+def create_application(data):
+    cursor = g.db.cursor()
+    insert_statement = "INSERT INTO application (name, phone) VALUES (\"{}\",\"{}\")".format(data["name"], data["phone"])
+    cursor.execute(insert_statement)
+
+# def create_response(data):
+#     cursor = g.db.cursor()
+#     insert_statement = "INSERT INTO application (name, phone) VALUES (\"{}\",\"{}\")".format(data.name, data.phone)
+#     cursor.execute(insert_statement)
+#     return g.db.insert_id()
+
+def create_candidate(id_position, data):
+    create_application(data)
+    cursor = g.db.cursor()
+    # TODO 
+    hash = "123456"
+    #insert_statement = "INSERT INTO app_pos (Aphone, Ppid, hash) VALUES (\"{}\", {}, \"{}\") ON DUPLICATE KEY UPDATE hash = \"{}\" WHERE Aphone = \"{}\" AND Ppid = {}".format(data["phone"], id_position, hash, hash, data["phone"], id_position)
+    insert_statement = "INSERT INTO app_pos (Aphone, Ppid, hash) VALUES (\"{}\", {}, \"{}\")".format(data["phone"], id_position, hash)
+    cursor.execute(insert_statement)        
+
+def update_candidate(id_position, id_candidate, data):
+    cursor = g.db.cursor()
+    update_statement = "UPDATE application SET name = \"{}\" WHERE phone=\"{}\"".format(data['name'], data['phone'])
+    cursor.execute(update_statement)
+
+def create_question(id_position, text):
+    questions = get_questions(id_position)
+    new_question = {"id_question":len(questions)+1, "text":text}
+    questions.append(new_question)
+    cursor = g.db.cursor()
+    insert_statement = "UPDATE positions SET questions='{}' WHERE pid={}".format(json.dumps(questions), id_position)
+    cursor.execute(insert_statement)
+
+def update_question(id_position, id_question, new_text):
+    questions = get_questions(id_position)
+    for question in questions:
+        question.update((id, new_text) for id, _ in question.iteritems() if id == id_question)
+    cursor = g.db.cursor()
+    update_statement = "UPDATE positions SET questions=\"{}\" WHERE pid={}".format(json.dumps(questions), id_position)
+    cursor.execute(update_statement)
+
+def create_position(title, progress, Iemail, description):
+    cursor = g.db.cursor()
+    insert_statement = "INSERT INTO positions (title, progress, Iemail, description) VALUES (\"{}\",{},\"{}\",\"{}\")".format(title, progress, Iemail, description)
+    cursor.execute(insert_statement)
+    return g.db.insert_id()
+
+def update_position(id_position, title, progress, Iemail, description):
+    cursor = g.db.cursor()
+    update_statement = "UPDATE positions SET title = \"{}\", progress = {}, Iemail = \"{}\", description = \"{}\" WHERE id_position = {}".format(id_position, title, progress, Iemail, description)
+    cursor.execute(update_statement)
+
+def create_interviewer(email, name):
+    cursor = g.db.cursor()
+    insert_statement = "INSERT INTO interviewer (email, name) VALUES (\"{}\",\"{}\")".format(email, name)
+    cursor.execute(insert_statement)
+    return g.db.insert_id()
+
+def insert_update_candidate(id_position, id_candidate, data):
+    if id_candidate == None:
+        create_candidate(id_position, data)
+    else:
+        update_candidate(id_position, id_candidate, data)
+
+def insert_update_question(id_position, id_question, text):
+    if id_candidate == None:
+        create_question(id_position, text)
+    else:
+        update_question(id_position, id_question, text)
+
+def remove_question(id_question, id_position):
+    questions = get_questions(id_position)
+    questions = [question for question in questions if not (question['id_question'] == id_question)] 
+    cursor = g.db.cursor()
+    update_statement = "UPDATE positions SET questions=\"{}\" WHERE pid={}".format(json.dumps(questions), id_position)
+    cursor.execute(update_statement)
+
+def get_questions(id_position):
+    return []
+
+def get_candidates(id_position):
+    pass
+
+def mock():
+    #create_interviewer("pelovett@gmail.com", "Peter Lovett")
+    id_position = create_position("Software Development", 0, "pelovett@gmail.com", "Code some stuff")
+    create_question(id_position, "Whats your name?")
+    create_question(id_position, "How old are you?")
+    data = {"phone":"15038808741", "name":"Peter Lovett"}
+    create_candidate(id_position, data)
+    data = {"phone":"15207885673", "name":"Paulo Soares"}
+    create_candidate(id_position, data)
+    g.db.commit()
+
+# @app.before_request
+# def before_request():
+#     open_db_connection()
+    
+# @app.after_request
+# def after_request(response):
+#     close_db_connection()
+#     return response
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    print_questions()
+    open_db_connection()
+    mock()
+    close_db_connection()
     if (request.method == 'POST'):
         email = request.form['email']
         password = request.form['password']
@@ -75,7 +174,19 @@ def manage_process():
         return redirect(url_for('index'))
 
     # show the form, it wasn't submitted
-    return render_template('manage_process.html')
+    hiring = {}
+    hiring["position"] = "Software Development"
+    hiring["description"] = "Position X"
+    hiring["id"] = 1123
+    hiring["questions"] = []
+    hiring["candidates"] = []
+    for i in range(4):        
+        question = {"id": i, "text": "Question " + str(i)}
+        hiring["questions"].append(question)
+    for i in range(2):        
+        candidate = {"id": i, "name": "Candidate" + str(i), "phone":"111111111111"}
+        hiring["candidates"].append(candidate)
+    return render_template('manage_process.html', hiring=hiring)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
